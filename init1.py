463dfe71cd0b660a07589d5c75a526beba8446f8
Lucrum <28992495+Lucrum@users.noreply.github.com>
2022-04-20 17:15:31 -0400

    Changed database name

--- init1.py
+++ init1.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
-                       db='airline',
+                       db='AirSystem',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
